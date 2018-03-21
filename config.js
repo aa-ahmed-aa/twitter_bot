e58d3f7fb9750e93a4314e56daf09861abab404b
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2018-03-21 19:04:11 +0200

    Iam not an idiot

--- config.js
+++ config.js
@@ -6,9 +6,9 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key:         'GCrKBXIeVi857kiEloAbU3cje',
-  consumer_secret:      'J438huNwCscwIIX7fb2j483xQrrQiOnYKZlAvi69am9tDIRDMr',
-  access_token:         '3078652071-vYVePMD47Ehv61zntHU9pqqAiv6iptNpvoDVUFA',
-  access_token_secret:  'O9tHRfPZGTfYRftXmdIbJtTb17hdryqcpeqqRrkg8eIIM',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
 }
