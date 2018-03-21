deccab36be178080db5c10766846801032d25133
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2018-03-21 19:01:51 +0200

    fixing some refs

---
+++ config.js
@@ -0,0 +1,14 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key:         'GCrKBXIeVi857kiEloAbU3cje',
+  consumer_secret:      'J438huNwCscwIIX7fb2j483xQrrQiOnYKZlAvi69am9tDIRDMr',
+  access_token:         '3078652071-vYVePMD47Ehv61zntHU9pqqAiv6iptNpvoDVUFA',
+  access_token_secret:  'O9tHRfPZGTfYRftXmdIbJtTb17hdryqcpeqqRrkg8eIIM',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+}
