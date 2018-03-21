cfe487a083816a9465973363c93df8dfcab4608f
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2018-03-21 18:49:56 +0200

    Refactoring the configs

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var Twit = require('twit')
+var Twit = require('twit');
 var request = require('request');
+var config = require('./config.js');
 fs = require('fs');
 
-var T = new Twit({
-  consumer_key:         'GCrKBXIeVi857kiEloAbU3cje',
-  consumer_secret:      'J438huNwCscwIIX7fb2j483xQrrQiOnYKZlAvi69am9tDIRDMr',
-  access_token:         '3078652071-vYVePMD47Ehv61zntHU9pqqAiv6iptNpvoDVUFA',
-  access_token_secret:  'O9tHRfPZGTfYRftXmdIbJtTb17hdryqcpeqqRrkg8eIIM',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
-});
+var T = new Twit( config );
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
