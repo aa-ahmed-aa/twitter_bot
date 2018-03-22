df168e08877ad5b04e2e0042f4b38a2902c268f1
Server <ahmedkhaled36@hotmail.com>
2018-03-22 20:35:33 +0200

    Read me and adding cron tab

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# twitter_bot
+# Twitter Bot
+this is a twitter bot that will tweet random verse from the holy quran every minut i build this bot using node and QuranApi
+
+## Install 
+-clone the repo via `git clone https://github.com/aa-ahmed-aa/twitter_bot`
+-go to your twitter api account and create app and generage accessTokens for your app
+-open `config.js` file and put your credential consumer_key, consumer_secret, access_token, access_token_secret
+-run the app using `node index.js`
+
+## Configuration
+cron package is working the same as cron tab in linux so if you want to change the factor of time see this graph to do so 
+  ┌────────────── second (optional)
+  │ ┌──────────── minute
+  │ │ ┌────────── hour
+  │ │ │ ┌──────── day of month
+  │ │ │ │ ┌────── month
+  │ │ │ │ │ ┌──── day of week
+  │ │ │ │ │ │
+  │ │ │ │ │ │
+  * * * * * *
+
+## Contribution
+feel free to star, fork and issuing about any question or enhancement
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var Twit = require('twit');
 var request = require('request');
 var config = require('./config.js');
-fs = require('fs');
+var fs = require('fs');
+var cron = require('node-cron');
+
 
 var T = new Twit( config );
+let randomSurah = getRandomInt(1,114);
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -11,25 +14,31 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-let randomSurah = getRandomInt(1,114);
-
 //get the sura
-request('http://api.alquran.cloud/surah/'+randomSurah, function (error, response, body) {
-  let result = JSON.parse(body);
-  maxNumberOfAyat  = result['data']['numberOfAyahs']; // Print the HTML for the Google homepage.
+function tweetRandomVerse()
+{
+    request('http://api.alquran.cloud/surah/'+randomSurah, function (error, response, body) {
+      let result = JSON.parse(body);
+      maxNumberOfAyat  = result['data']['numberOfAyahs']; // Print the HTML for the Google homepage.
 
-  //get the aya
-  let randomNumberofAya = getRandomInt(1,maxNumberOfAyat);
-  request('http://api.alquran.cloud/ayah/'+randomSurah+':'+randomNumberofAya, function (error, response, body) {
-  		let result = JSON.parse(body);
-  		
-  		//get the text of the aya
-  		let randomAya = result['data']['text'];
+      //get the aya
+      let randomNumberofAya = getRandomInt(1,maxNumberOfAyat);
+      request('http://api.alquran.cloud/ayah/'+randomSurah+':'+randomNumberofAya, function (error, response, body) {
+      		let result = JSON.parse(body);
+      		
+      		//get the text of the aya
+      		let randomAya = result['data']['text'];
 
-  		T.post('statuses/update', { status: randomAya }, function(err, data, response) {
-		  console.log(data)
-		});
+      		T.post('statuses/update', { status: randomAya }, function(err, data, response) {
+    		  console.log(data)
+    		});
 
-  });
+      });
 
+    });
+}
+
+cron.schedule('* * * * *', function(){
+    tweetRandomVerse();
+    console.log("i tweeted random verse chell up");
 });
